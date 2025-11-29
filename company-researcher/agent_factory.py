cf6f08cc27979ba4a78821a336d2c9b7c1439167
mischlox <michael-schlosser99@gmx.de>
2025-11-29 14:34:26 +0100

    Add query checker guardrail

--- company-researcher/agent_factory.py
+++ company-researcher/agent_factory.py
@@ -3,10 +3,15 @@ import os
 from google.adk.agents import Agent
 
 GEMINI_MODEL_NAME = os.getenv("GEMINI_MODEL_NAME")
-if not GEMINI_MODEL_NAME:
-    raise ValueError("GEMINI_MODEL_NAME must be set.")
 
-def create_agent(name, instruction, output_schema = None, tools = None) -> Agent:
+def create_agent(name, instruction, output_schema=None, tools=None, before_model_callback=None) -> Agent:
     if not GEMINI_MODEL_NAME:
         raise ValueError("GEMINI_MODEL_NAME must be set.") 
-    return Agent(name=name, model=GEMINI_MODEL_NAME, instruction=instruction, output_schema=output_schema, tools=tools or [])
+    
+    return Agent(name=name,
+                 model=GEMINI_MODEL_NAME,
+                 instruction=instruction,
+                 output_schema=output_schema,
+                 tools=tools or [],
+                 before_model_callback=before_model_callback
+                 )
