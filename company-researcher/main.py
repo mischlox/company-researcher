4a4cb46cf8fc4dbc24076b3414a73d401e0caae9
mischlox <michael-schlosser99@gmx.de>
2025-12-01 19:15:41 +0100

    Remove comment

--- company-researcher/main.py
+++ company-researcher/main.py
@@ -11,17 +11,7 @@ async def run(query, history):
         yield update
 
 def main():
-    # with gr.Blocks(theme=gr.themes.Glass(primary_hue="sky")) as ui:
-    #     gr.Markdown("# Deep Research")
-    #     query_textbox = gr.Textbox(label="Enter topic to research", placeholder="Type your query here...", interactive=True)
-    #     run_button = gr.Button("Run Research", variant="primary", elem_id="run-button")
 
-    #     report_output = gr.Markdown(label="Report", value="Waiting for query...")
-
-    #     # Link the button and enter key to the async generator
-    #     run_button.click(fn=run, inputs=query_textbox, outputs=report_output)
-    #     query_textbox.submit(fn=run, inputs=query_textbox, outputs=report_output)
-    # ui.launch(inbrowser=True)
     with gr.Blocks(fill_height=True) as ui:
         gr.ChatInterface(
             run,
